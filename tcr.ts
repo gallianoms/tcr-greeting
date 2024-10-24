2da013b6600e2da0c712625fbf7065e0d4963591
gallianoms <ggallianoms@gmail.com>
2024-10-24 23:37:43 +0200

    TCR: Tests passed

--- tcr.ts
+++ tcr.ts
@@ -5,7 +5,7 @@ exec('npm test', (err) => {
   if (err) {
     // Si fallan los tests, revertimos cambios
     console.log('Tests failed. Reverting...')
-    // exec('git reset --hard')
+    exec('git reset --hard')
   } else {
     // Si los tests pasan, hacemos commit
     console.log('Tests passed. Committing...')
